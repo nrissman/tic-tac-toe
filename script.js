aa979aa7146c655f2e89ab2be92c7547a4b18875
nrissman <nrissman90@gmail.com>
2022-06-10 19:09:23 -0400

    finished grid and b1

---
+++ script.js
@@ -0,0 +1,21 @@
+// let b1 = document.getElementById('b1')
+
+
+// b1.addEventListener('click', function (event){
+//     console.log('hello')
+// })
+
+
+let b1= document.getElementById('b1')
+
+
+// //click on my I love cats div and have it say hello
+// helloDiv.addEventListener('click', function (event){
+//     console.log('Hellooo thereee')
+//     //look at where the event took place
+//     console.log(event.target)
+// })
+b1.addEventListener('click', function (event){
+ console.log('helllo')
+
+})
