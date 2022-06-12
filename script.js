ac58158f7b191ff54b312c650e00d854b92c39b6
nrissman <nrissman90@gmail.com>
2022-06-12 13:13:27 -0400

    all things working

--- script.js
+++ script.js
@@ -7,10 +7,11 @@
 let playerTurn = true
 //keep track of turns to end game in tie 
 let turns = 0 
+let playerDisplay = 'O'
  
 
 
-
+let endGame= document.getElementById('winner')
 let b1= document.getElementById('b1')
 let b2= document.getElementById('b2')
 let b3= document.getElementById('b3')
@@ -29,39 +30,46 @@ let truthArray = [true, true, true, true, true, true, true, true, true]
 
 
 b1.addEventListener('click', function (event){
-    if (truthArray[0] === true){
-        if (playerTurn === true){
-            console.log('helllo')
-            b1.innerText = ('X')  
-            playerTurn = false
-        }else if (playerTurn === false){
-            b1.innerText = ('O')
-            playerTurn = true  
-        } 
-        turns = turns +1
-        truthArray[0] = false
-    }   checkWin ()
+    if (endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
+        if (truthArray[0] === true){
+            if (playerTurn === true){
+                console.log('helllo')
+                b1.innerText = ('X')  
+                playerTurn = false
+            }else if (playerTurn === false){
+                b1.innerText = ('O')
+                playerTurn = true  
+            } 
+            turns = turns +1
+            truthArray[0] = false
+        }   checkWin ()
+    }
+   
     
       
 })
 
 b2.addEventListener('click', function (event){
-    if (truthArray[1] === true){
-        if (playerTurn === true){
-            console.log('helllo')
-            b2.innerText = ('X')  
-            playerTurn = false
-        }else if (playerTurn === false){
-            b2.innerText = ('O')
-            playerTurn = true
-        } 
-        turns = turns +1
-        truthArray[1] = false
-    }   checkWin ()
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
+        if (truthArray[1] === true){
+            if (playerTurn === true){
+                console.log('helllo')
+                b2.innerText = ('X')  
+                playerTurn = false
+            }else if (playerTurn === false){
+                b2.innerText = ('O')
+                playerTurn = true
+            } 
+            turns = turns +1
+            truthArray[1] = false
+        }   checkWin ()   
+    }
+    
     
 })
 
 b3.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[2] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -74,9 +82,11 @@ b3.addEventListener('click', function (event){
         turns = turns +1
         truthArray[2] = false
     }   checkWin ()
+}
 })
 
 b4.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[3] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -89,9 +99,11 @@ b4.addEventListener('click', function (event){
         turns = turns +1
         truthArray[3] = false
     }   checkWin ()
+}
 })
 
 b5.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[4] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -104,9 +116,11 @@ b5.addEventListener('click', function (event){
         turns = turns +1
         truthArray[4] = false
     }   checkWin ()
+}
 })
 
 b6.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[5] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -119,9 +133,11 @@ b6.addEventListener('click', function (event){
         turns = turns +1
         truthArray[5] = false
     }   checkWin ()
+}
 })
 
 b7.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[6] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -134,9 +150,11 @@ b7.addEventListener('click', function (event){
         turns = turns +1
         truthArray[6] = false
     }   checkWin ()
+}
 })
 
 b8.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[7] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -149,9 +167,11 @@ b8.addEventListener('click', function (event){
         turns = turns +1
         truthArray[7] = false
     }   checkWin ()
+}
 })
 
 b9.addEventListener('click', function (event){
+    if(endGame.innerText!= 'WINNER WINNER CHICKEN DINNER PLAYER `${} HAS WON!'){
     if (truthArray[8] === true){
         if (playerTurn === true){
             console.log('helllo')
@@ -164,6 +184,7 @@ b9.addEventListener('click', function (event){
         turns = turns +1
         truthArray[8] = false
     }   checkWin ()
+}
 })
 
 
@@ -177,44 +198,39 @@ function checkWin (){
         b1.innerText != "" && b1.innerText === b5.innerText && b1.innerText === b9.innerText ||
         b3.innerText != "" && b3.innerText === b5.innerText && b3.innerText === b7.innerText
     ){
-        document.getElementById("winner").innerText = "Player ${} has won!"
+        //document.getElementById("winner").innerText = "Player ${} has won!"
+        endGame.innerText = 'WINNER WINNER CHICKEN DINNER PLAYER ' + playerDisplay + ' HAS WON!'
         console.log("Winner, Winner, Chicken Dinner!")
 
     }
         if (turns === 9) {
             document.getElementById("winner").innerText = "It's a Tie. Click Restart Game to play again!"
         } if (turns === 1) {
-            document.getElementById("playerturn").innerText = "Player 2 Turn"
+            document.getElementById("playerturn").innerText = "Player O Turn"
+            playerDisplay = 'O'
         } if (turns === 2) {
-            document.getElementById("playerturn").innerText = "Player 1 Turn"
+            document.getElementById("playerturn").innerText = "Player X Turn"
+            playerDisplay = 'X'
         } if (turns === 3) {
-            document.getElementById("playerturn").innerText = "Player 2 Turn"
+            document.getElementById("playerturn").innerText = "Player O Turn"
+            playerDisplay = 'O'
         } if (turns === 4) {
-            document.getElementById("playerturn").innerText = "Player 1 Turn"
+            document.getElementById("playerturn").innerText = "Player X Turn"
+            playerDisplay = 'X'
         } if (turns === 5) {
-            document.getElementById("playerturn").innerText = "Player 2 Turn"
+            document.getElementById("playerturn").innerText = "Player O Turn"
+            playerDisplay = 'O'
         } if (turns === 6) {
-            document.getElementById("playerturn").innerText = "Player 1 Turn"
+            document.getElementById("playerturn").innerText = "Player X Turn"
+            playerDisplay = 'X'
         } if (turns === 7) {
-            document.getElementById("playerturn").innerText = "Player 2 Turn"
+            document.getElementById("playerturn").innerText = "Player O Turn"
+            playerDisplay = 'O'
         } if (turns === 8) {
-            document.getElementById("playerturn").innerText = "Player 1 Turn"
-        } 
+            document.getElementById("playerturn").innerText = "Player X Turn"
+        }   playerDisplay = 'X'
 }
 
-function endGame () {
-    if (b1.innerText != "" && b1.innerText === b2.innerText && b1.innerText === b3.innerText ||
-        b4.innerText != "" && b4.innerText === b5.innerText && b4.innerText === b6.innerText ||
-        b7.innerText != "" && b7.innerText === b8.innerText && b7.innerText === b9.innerText ||
-        b1.innerText != "" && b1.innerText === b4.innerText && b1.innerText === b7.innerText ||
-        b2.innerText != "" && b2.innerText === b5.innerText && b2.innerText === b8.innerText ||
-        b3.innerText != "" && b3.innerText === b6.innerText && b3.innerText === b9.innerText ||
-        b1.innerText != "" && b1.innerText === b5.innerText && b1.innerText === b9.innerText ||
-        b3.innerText != "" && b3.innerText === b5.innerText && b3.innerText === b7.innerText
-    ) {
-        document.getElementsByClassName("grid").removeEventListener
-    }
-}
 
 
 
